8308a4c93b6c529b830865559b129ef88499a4a2
Artem Sapegin <asapegin@wayfair.com>
2018-08-02 08:55:22 +0200

    Chore: Flow, browserslist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// @flow
 import React from 'react';
 import { Link as LinkBase, QuotedLink as QuotedLinkBase } from 'tamia';
 import { Link as GatbsyLinkBase } from 'gatsby';
 
-const isExternalLink = href => {
+type Props = {
+	href?: string,
+};
+
+const isExternalLink = (href: string): boolean => {
 	const protocol = href && href.split(':').shift();
 	return protocol === 'https' || protocol === 'http' || protocol === 'mailto';
 };
@@ -10,14 +15,14 @@ const isExternalLink = href => {
 export const GatsbyLink = LinkBase.withComponent(GatbsyLinkBase);
 export const QuotedGatsbyLink = QuotedLinkBase.withComponent(GatbsyLinkBase);
 
-export const Link = ({ href, ...props }) =>
+export const Link = ({ href, ...props }: Props) =>
 	isExternalLink(href) ? (
 		<LinkBase href={href} {...props} />
 	) : (
 		<GatsbyLink to={href} {...props} />
 	);
 
-export const QuotedLink = ({ href, ...props }) =>
+export const QuotedLink = ({ href, ...props }: Props) =>
 	isExternalLink(href) ? (
 		<QuotedLinkBase href={href} {...props} />
 	) : (
