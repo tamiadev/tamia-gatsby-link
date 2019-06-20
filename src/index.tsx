b146bfe3e586241dfd2c38c7d3b24a9926ffe6d8
Artem Sapegin <asapegin@wayfair.com>
2019-06-20 13:18:09 +0200

    Fix: Convert to TypeScript and publish TypeScript types

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-// @flow
 import React from 'react';
 import { Link as LinkBase, QuotedLink as QuotedLinkBase } from 'tamia';
 import { Link as GatbsyLinkBase } from 'gatsby';
 
-type Props = {
-	href?: string,
-};
+/* eslint-disable react/no-multi-comp */
 
-const isExternalLink = (href: string): boolean => {
+type Props = React.ComponentPropsWithRef<typeof LinkBase> &
+	React.ComponentPropsWithRef<typeof GatbsyLinkBase>;
+
+const isExternalLink = (href: string = ''): boolean => {
 	const protocol = href && href.split(':').shift();
 	return protocol === 'https' || protocol === 'http' || protocol === 'mailto';
 };
@@ -15,20 +15,22 @@ const isExternalLink = (href: string): boolean => {
 export const GatsbyLink = LinkBase.withComponent(GatbsyLinkBase);
 export const QuotedGatsbyLink = QuotedLinkBase.withComponent(GatbsyLinkBase);
 
-export const Link = React.forwardRef(
+export const Link = React.forwardRef<HTMLAnchorElement, Props>(
 	({ href, ...props }: Props, ref) =>
 		isExternalLink(href) ? (
 			<LinkBase href={href} ref={ref} {...props} />
 		) : (
+			// @ts-ignore No idea how to fix ref here
 			<GatsbyLink to={href} ref={ref} {...props} />
 		)
 );
 
-export const QuotedLink = React.forwardRef(
+export const QuotedLink = React.forwardRef<HTMLAnchorElement, Props>(
 	({ href, ...props }: Props, ref) =>
 		isExternalLink(href) ? (
 			<QuotedLinkBase href={href} ref={ref} {...props} />
 		) : (
+			// @ts-ignore No idea how to fix ref here
 			<QuotedGatsbyLink to={href} ref={ref} {...props} />
 		)
 );
